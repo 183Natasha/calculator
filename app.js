cb12431285868d16598c50efae4a428df154824d
183Natasha <168049595+183Natasha@users.noreply.github.com>
2025-03-07 21:20:00 +0500

    test

--- app.js
+++ app.js
@@ -1,22 +1,73 @@
 console.log('test')
 
-let button0 = Number(document.getElementById("button0").innerHTML)
-let button1 = Number(document.getElementById("button1").innerHTML)
-let button2 = Number(document.getElementById("button2").innerHTML)
-let button3 = Number(document.getElementById("button3").innerHTML)
-let button4 = Number(document.getElementById("button4").innerHTML)
-let button5 = Number(document.getElementById("button5").innerHTML)
-let button6 = Number(document.getElementById("button6").innerHTML)
-let button7 = Number(document.getElementById("button7").innerHTML)
-let button8 = Number(document.getElementById("button8").innerHTML)
-let button9 = Number(document.getElementById("button9").innerHTML)
+let calculator = document.getElementById('calculator');
+let display = document.getElementById('display');
 
-let button_logo = Number(document.getElementById("logo").innerHTML)
-let button_dot = Number(document.getElementById("dot").innerHTML)
-let button_divide = Number(document.getElementById("divide").innerHTML)
-let button_myltiply = Number(document.getElementById("myltiply").innerHTML)
-let button_minus = Number(document.getElementById("minus").innerHTML)
-let button_plus = Number(document.getElementById("plus").innerHTML)
-let button_equal = Number(document.getElementById("equal").innerHTML)
-// button1 = Number(button1.innerHTML)
-console.log(typeof button1, button1)
+display.innerHTML = "0"
+display.value= ' '
+
+
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+calculator.addEventListener('click', function (event) {
+// console.log(event.target.tagName)
+
+    if (event.target.tagName === "BUTTON"  || event.target.tagName=="IMG") {
+
+        if (event.target.value === "Clear"  || event.target.tagName=="IMG") {
+            console.log(10)
+            display.innerHTML = '0'
+            currentInput = '';
+            operator = '';
+            previousInput = '';
+        } else {
+            display.value += event.target.value;
+            display.innerHTML = display.value
+        }
+        if (event.target.value === "="){
+
+        }
+
+
+    }
+
+    // if (event.target.tagName === "Clear") {
+    //     display.value = '0'
+    //     display.innerHTML = display.value
+    //     currentInput = '';
+    //     operator = '';
+    //     previousInput = '';
+    // }
+    //     if (event.target.tagName === '=') {
+    //         if (currentInput && previousInput && operator) {
+    //             currentInput = calculate(previousInput, currentInput, operator);
+    //             display.value = currentInput;
+    //             display.innerText = currentInput;
+    //             console.log(currentInput)
+    //             previousInput = '';
+    //             operator = '';
+    //         }
+    //     }
+
+
+
+})
+
+function calculate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+    }
+}    
